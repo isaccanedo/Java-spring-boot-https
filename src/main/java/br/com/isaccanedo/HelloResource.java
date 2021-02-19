db5041300e00f999d8411cf5697ee849b086df17
isaccanedo <isaccanedo@gmail.com>
2021-02-18 22:44:07 -0300

    Initial commit

---
+++ src/main/java/br/com/isaccanedo/HelloResource.java
@@ -0,0 +1,15 @@
+package br.com.isaccanedo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloResource {
+
+    @GetMapping
+    public String hello() {
+        return "Olá Isac Canedo";
+    }
+}
